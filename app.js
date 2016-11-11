3c26ac28d172358544334b3a1defdefbe2f6010b
Narendra Soni <narendrasoni2@gmail.com>
2016-11-11 11:10:09 +0800

    Improved project structure

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ var cookieParser = require('cookie-parser');
 var cookieEncrypter = require('cookie-encrypter');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var config = require('./config/config');
+
+
 var secretKey = 'foobarbaz12345';
 var app = express();
 
@@ -27,39 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.use('/assets',express.static(path.join(__dirname, 'public/stylesheets')));
 
 
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
+require('./config/routes')(app);
+require('./config/express')(app);
 
 module.exports = app;
